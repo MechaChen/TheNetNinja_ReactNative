0128fc60cd648201283365d30ee7b9a3714e69a2
Benson Chen <tumlivein@gmail.com>
2019-12-11 12:04:44 +0800

    14. Flexbox Basics: Sandbox example

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyb
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
+import Sandbox from './components/sandbox';
 
 export default function App() {
     const [todos, setTodos] = useState([
@@ -23,7 +24,8 @@ export default function App() {
                 'Todos must be over 3 chars long', 
                 [
                     { text: 'Understood', onPress: () => console.log('alert closed') }
-                ]);
+                ]
+            );
         }
 
         setTodos((prevTodos) => {
@@ -32,25 +34,26 @@ export default function App() {
     }
 
     return (
-        <TouchableWithoutFeedback onPress={() => {
-            Keyboard.dismiss();
-            console.log('dismissed keyboard');
-        }}>
-            <View style={styles.container}>
-                <Header />
-                <View style={styles.content}>
-                    <AddTodo submitHandler={submitHandler}/>
-                    <View style={styles.list}>
-                        <FlatList
-                            data={todos}
-                            renderItem={({ item }) => (
-                                <TodoItem item={item} pressHandler={pressHandler}/>
-                            )}
-                        />
-                    </View>
-                </View>
-            </View>
-        </TouchableWithoutFeedback>
+        <Sandbox />
+        // <TouchableWithoutFeedback onPress={() => {
+        //     Keyboard.dismiss();
+        //     console.log('dismissed keyboard');
+        // }}>
+        //     <View style={styles.container}>
+        //         <Header />
+        //         <View style={styles.content}>
+        //             <AddTodo submitHandler={submitHandler}/>
+        //             <View style={styles.list}>
+        //                 <FlatList
+        //                     data={todos}
+        //                     renderItem={({ item }) => (
+        //                         <TodoItem item={item} pressHandler={pressHandler}/>
+        //                     )}
+        //                 />
+        //             </View>
+        //         </View>
+        //     </View>
+        // </TouchableWithoutFeedback>
     );
 }
 
